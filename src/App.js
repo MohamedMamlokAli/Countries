3812a3656b2d52e5cd271f624483bb2dae7da6bf
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2021-10-27 07:07:37 +0200

    Fixed errors from api changes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React,{useEffect,useState} from "react"
-import axios from "axios"
-import Header from "./components/Header"
-import SearchFilter from "./components/SearchFilter"
-import {BrowserRouter,Switch,Route} from "react-router-dom"
-import CountryDetails from "./components/CountryDetails"
+import React, { useEffect, useState } from 'react';
+import Header from './components/Header';
+import SearchFilter from './components/SearchFilter';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
-  const [countries,setCountries] = useState([])
-
+  const [countries, setCountries] = useState([]);
+  const getCountries = async (apiLink) => {
+    const res = await fetch(apiLink);
+    const data = await res.json();
+    setCountries(data);
+  };
   useEffect(() => {
-    new axios.get("https://restcountries.eu/rest/v2/all" ).then(res=>setCountries(res.data))
-  }, [])
+    try {
+      getCountries('https://restcountries.com/v2/all');
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
-    <div id="app" className="App">
-      <div className="bg-white text-gray-800 dark:text-gray-100  dark:bg-gray-800 h-screen">
-      <Header />
-      <BrowserRouter>
-      <Switch>
-        <Route path="/" render={() => <SearchFilter countries = {countries} />} exact/> 
-        <Route path ="/:country" component={CountryDetails}/>
-      </Switch>
-      </BrowserRouter>
-      
+    <div id='app' className='App'>
+      <div className='bg-white text-gray-800 dark:text-gray-100  dark:bg-gray-800 h-screen'>
+        <Header />
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path='/'
+              render={() => <SearchFilter countries={countries} />}
+              exact
+            />
+            <Route path='/:country' component={CountryDetails} />
+          </Switch>
+        </BrowserRouter>
       </div>
     </div>
   );
