aac05cc7a8372ec5fb990cb97713f91cb9f80201
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2021-10-27 07:19:48 +0200

    Changed title of country card

--- src/components/CardInd.js
+++ src/components/CardInd.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -8,48 +8,41 @@ import Typography from '@material-ui/core/Typography';
 const useStyles = makeStyles((theme) => ({
   root: {
     minWidth: 345,
-    maxWidth:345,
-    height:400,
-    borderRadius:'10px',
-    marginTop:"2.5rem"
+    maxWidth: 345,
+    height: 400,
+    borderRadius: '10px',
+    marginTop: '2.5rem',
   },
   media: {
     height: 171,
     paddingTop: '56.25%', // 16:9
   },
-  content:{
-    
-  }
+  content: {},
 }));
 
 const CardInd = (props) => {
- const {flag,name,population,region,capital} = props
-   const classes = useStyles();
+  const { flag, name, population, region, capital } = props;
+  const classes = useStyles();
 
- return (
-    <Card className={classes.root} >
-      <CardMedia
-        className={classes.media}
-        image={flag}
-        title="Paella dish"
-      />
-      <CardContent className="bg-white text-gray-800 dark:text-gray-100 dark:bg-gray-600 h-full" >
-        <Typography variant="h5" component="p">
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image={flag} title={name} />
+      <CardContent className='bg-white text-gray-800 dark:text-gray-100 dark:bg-gray-600 h-full'>
+        <Typography variant='h5' component='p'>
           {name}
         </Typography>
-        <Typography variant="h6" component="p">
+        <Typography variant='h6' component='p'>
           Population: {population}
         </Typography>
-        <Typography variant="h6" component="p">
+        <Typography variant='h6' component='p'>
           Region: {region}
         </Typography>
-        <Typography variant="h6" component="p">
+        <Typography variant='h6' component='p'>
           Capital: {capital}
         </Typography>
-
       </CardContent>
     </Card>
- )
-}
+  );
+};
 
-export default CardInd
+export default CardInd;
